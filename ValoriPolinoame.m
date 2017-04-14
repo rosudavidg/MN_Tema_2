a0e7bde9d157ce162f063db4f0501a76bf6479bb
rosudavidg <rosudavidgabriel@gmail.com>
2017-04-14 20:52:57 +0300

    first ex

--- ValoriPolinoame.m
+++ ValoriPolinoame.m
@@ -1,7 +1,12 @@
 function P = ValoriPolinoame(d, s, val_lambda)
 
+  n = length(d);
 
+  P(1) = 1;
+  P(2) = d(1) - val_lambda;
 
-
+  for i = [2: n]
+    P(i + 1) = (d(i) - val_lambda) * P(i) - (s(i - 1)).^2 * P(i - 1);
+    endfor
 
 endfunction
