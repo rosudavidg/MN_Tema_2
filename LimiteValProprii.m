5062bc17b2dd5d7b8c417b44519a41545c56eaa9
rosudavidg <rosudavidgabriel@gmail.com>
2017-04-14 22:47:54 +0300

    ex 3

--- LimiteValProprii.m
+++ LimiteValProprii.m
@@ -1,6 +1,19 @@
-function [limtia_inf, limita_sup] = LimiteValProprii(d, s)
+function [limita_inf, limita_sup] = LimiteValProprii(d, s)
 
-limita_inf = min(d - abs(s));
-limita_sup = max(d + abs(s));
+n = length(d);
 
-enfunction
+t(1) = abs(s(1));
+t(n) = abs(s(n - 1));
+
+for i = 2: n - 1
+  t(i) = abs(s(i - 1)) + abs(s(i));
+endfor
+
+for i = 1:n
+  d_min(i) = d(i) - t(i);
+  d_max(i) = d(i) + t(i);
+endfor
+
+limita_inf = min(d_min);
+limita_sup = max(d_max);
+endfunction
